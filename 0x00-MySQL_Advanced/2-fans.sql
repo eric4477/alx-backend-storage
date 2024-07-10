00706ebf49834e1821cbfb33f13a47927aa3951d
eric4477 <ericgeorge4477@gmail.com>
2024-07-10 21:18:11 +0300

    refactor

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,12 +1,5 @@
 -- Ranks country origins of bands, ordered by the number of (non-unique) fans
 -- Column names: origin and nb_fans
 
-SELECT
-    origin,
-    SUM(nb_fans) AS total_fans
-FROM
-    metal_bands
-GROUP BY
-    origin
-ORDER BY
-    total_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
